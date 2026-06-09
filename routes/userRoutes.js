8c7118a9acfd8472f980906fa6cb4640381213d4
saleh <ntegesaleh@gmail.com>
2026-06-09 23:12:57 +0300

    Add Swagger request body documentation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,9 +10,43 @@ const {
 } = require("../controllers/userController");
 
 router.get("/", getAllUsers);
+
 router.get("/:id", getUserById);
-router.post("/", createUser);
-router.put("/:id", updateUser);
+
+router.post(
+  "/",
+  /* #swagger.tags = ['Users']
+     #swagger.description = 'Create a new user'
+     #swagger.parameters['body'] = {
+       in: 'body',
+       required: true,
+       schema: {
+         $name: 'Saleh',
+         $email: 'saleh@example.com',
+         $age: 25
+       }
+     }
+  */
+  createUser
+);
+
+router.put(
+  "/:id",
+  /* #swagger.tags = ['Users']
+     #swagger.description = 'Update a user'
+     #swagger.parameters['body'] = {
+       in: 'body',
+       required: true,
+       schema: {
+         $name: 'Updated Saleh',
+         $email: 'updated@example.com',
+         $age: 26
+       }
+     }
+  */
+  updateUser
+);
+
 router.delete("/:id", deleteUser);
 
 module.exports = router;
